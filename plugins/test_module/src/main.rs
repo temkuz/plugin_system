c8527671ff77c63c9294514ee51c00e26683813a
temkuz <temkuz@yandex.ru>
2025-11-24 18:02:58 +0500

    refactoring: change modules for plugins

---
+++ plugins/test_module/src/main.rs
@@ -0,0 +1,16 @@
+use plugin::{Handler, Request, Response};
+
+/// An example of a function that ignores input parameters and always returns success response
+fn ping(request: Request) -> Response {
+    let value = serde_json::to_value("pong").unwrap();
+    Response::success(value, request.id)
+}
+
+fn main() {
+    let request = Request::read_stdin();
+
+    let mut handler = Handler::new();
+    handler.register("ping", ping);
+
+    handler.execute(request).write();
+}
